92a72332775374629db279117943df5ad73a4470
“RestuArdi03” <restuardi03@gmail.com>
2025-11-02 17:37:56 +0700

    Menambahkan tabel untuk database

--- database/migrations/2025_11_02_101927_create_notulen_table.php
+++ database/migrations/2025_11_02_101927_create_notulen_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('notulen', function (Blueprint $table) {
             $table->id();
             $table->string('judul');
+            $table->text('catatan');
             $table->foreignId('kegiatan_id')->constrained('kegiatan')->onDelete('cascade');
             $table->foreignId('users_id')->constrained('users')->onDelete('restrict');
             $table->timestamps();
