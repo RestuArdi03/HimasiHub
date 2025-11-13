16f46916a78101efd53dacbcab86de780269c3bd
“RestuArdi03” <restuardi03@gmail.com>
2025-11-13 22:11:12 +0700

    sedikit update tadi lupa hehe

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -56,7 +56,6 @@
                 </li>
 
                 {{-- KEUANGAN --}}
-                <li class="sidebar-title">Keuangan</li>
                 <li class="sidebar-item {{ request()->routeIs('backend.saldo.*') || request()->routeIs('backend.transaksi.*') || request()->routeIs('backend.kas.*') ? 'active' : '' }} ">
                     <a href="{{ route('backend.saldo.index') }}" class='sidebar-link'>
                         <i class="bi bi-cash"></i>
